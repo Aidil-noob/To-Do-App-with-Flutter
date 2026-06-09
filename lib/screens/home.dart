1cf9dfe35bd014d905a40d5bd7fd679f4936fd75
Aidil <refandyakbar1709@gmail.com>
2026-06-09 20:41:59 +0700

    update lib/screens/home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -210,7 +210,7 @@ class _HomeState extends State<Home> {
           width: 40,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: Image.asset('gambar/Ppaidil.jpeg'),
+            child: Image.asset('gambar/Ppaidil.jpg'),
           ),
         ),
       ]),
