c22fc73fee15d798c501e3f9c1693239efba7a39
Aidil <refandyakbar1709@gmail.com>
2026-06-09 21:15:56 +0700

    update lib/model/todo.dart

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -10,13 +10,6 @@ class ToDo {
   });
 
   static List<ToDo> todoList() {
-    return [
-      ToDo(id: '01', todoText: 'Lari Pagi', isDone: true ),
-      ToDo(id: '02', todoText: 'Belanja', isDone: true ),
-      ToDo(id: '03', todoText: 'Belajar', ),
-      ToDo(id: '04', todoText: 'Pergi Sekolah', ),
-      ToDo(id: '05', todoText: 'Buat Website', ),
-      ToDo(id: '06', todoText: 'Makan Malam', ),
-    ];
+    return [];
   }
 }
